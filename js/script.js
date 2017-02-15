064322cc250b5d230b6dd30ef940012002390c34
Kari Schendel <firefaded@yahoo.com>
2017-02-15 17:23:36 -0600

    completed assignment

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function () {
+    $("img").hide();
+    $("h2").after("<p>Please click on an item to see its image.</p>");
+
+    $("span").on("click",function () {
+        $("aside").empty();
+        let itemImage = ($(this).next().attr("src"));
+        $("aside").append('<img src="' + itemImage + '">');
+    })
+
+});
