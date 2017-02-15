a41a2fc1e77afccc07b47227cd376aca48b9f5a4
Josh Hight <HIGHJOS@chrobinson.com>
2017-02-15 17:08:05 -0600

    week 5 assignment

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//ready function
+
+$(document).ready(function(){
+                                            
+       $("img").hide();           
+       $("h2").after("<h3>You can click on an item to see an image.<h3>");    
+       $("span").on("click", function() {
+       $("aside").empty();
+       let picture =$(this).next().attr("src");
+       $("aside").append('<img src="' + picture + '">');
+                  });
+                    });
+                  
+                  
+                  
+                  
+                  
+
+                  
+//                  # jQuery Moonflower's Coffee Menu
+//Completing this assignment depends on knowing how to use:
+//
+//- a CDN to include jQuery in your page
+//- the jQuery ready function to keep your variables out of global scope
+//- jQuery methods for selecting and navigating the DOM Tree
+//- jQuery methods for working with attributes
+//- jQuery methods for creating elements and appending it to the DOM
+//- jQuery methods for handling events
+//
+//## Moonflower’s Coffee Shop
+//Open the web page in a browser.  You’ll see that it loads a page with an image for each item on the menu.  
+//In the spirit of progressive enhancement, the page may not be amazingly beautiful, but all the info is visible without any JavaScript.
+//
+//For this assignment, you will transform the page with JavaScript & jQuery, altering the DOM so that when the page loads, all images are hidden.  Add text under the “Menu” heading to indicate that you can click on an item to see an image.  Then when an item is clicked,  such as the Cappuccino or the Latte, the corresponding image should be shown off to the side.
+//
+//If you look at the HTML provided, you’ll see an aside tag near the bottom.  This where we will add the image to display on the right side.  You can show one of the menu item images by adding an image with values for src and alt similar to the “hidden” image in the menu list.
+//
+//All of this magic is done in just a few lines of code using jQuery!  
